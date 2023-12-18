3e9dca812d1cd5a6737af684c7017a4dea414df9
LuisLopesDEV <LuisLopesDEV1@gmail.com>
2023-12-18 12:44:18 -0300

    novo projeto - Tabuada

--- Exercicios/Tabuada/script.js
+++ Exercicios/Tabuada/script.js
@@ -1,19 +1,22 @@
 function calcular() {
     let num = document.getElementById('n')
     let res = document.getElementById('res')
-    let n = Number(num.value)
+   
 
 
     if (num.value.length == 0) {
         alert('Insira um número')
-    } else(n > 0);{
-        for (let c = n; c <= n * 10; c += n) {
-            res.innerHTML += `${c}`
+    } else{
+        let n = Number(num.value)
+        let c=1
+        res.innerHTML=''
+        while(c<=13){
+            let item=document.createElement('option')
+            item.text=`${n} x ${c} = ${n*c}`
+            item.value=`op${c}`
+            res.appendChild(item)
+            c++
         }
-        if(n<0){
-            for(let c=n; c<n*10; c-=n){
-                res.innerHTML+=`${c}`
-            }
-        }
+        
     }
 }
